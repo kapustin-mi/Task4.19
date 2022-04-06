e3c1925ea524f22bc5bd14df98dcf5fabea7cc57
Максим Капустин <maximkapus7in@yandex.ru>
2022-04-06 12:39:17 +0300

    Add files via upload

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -24,6 +24,23 @@ public class Utils {
         return cutBorders;
     }
 
+    public static List<Interval> readIntervals(File file) throws FileNotFoundException {
+        List<String> lines = readLines(file);
+
+        if (lines.size() == 0) {
+            return null;
+        }
+
+        List<Interval> intervals = new ArrayList<>();
+
+        for (String line : lines) {
+            String[] points = line.split(", ");
+            intervals.add(new Interval(Integer.parseInt(points[0]), Integer.parseInt(points[1])));
+        }
+
+        return intervals;
+    }
+
     public static List<String> readLines(File file) throws FileNotFoundException {
         List<String> lines = new ArrayList<>();
         Scanner scn = new Scanner(file);
